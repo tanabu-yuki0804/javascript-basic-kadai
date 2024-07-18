bfade9d261ab7f0be6aeb35e30b31f678cb543a2
tanabu-yuki0804 <osarunojoji@outlook.com>
2024-07-18 14:15:31 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,11 @@
+//変数の宣言：体重（kg）の代入
+let userWeight = 68;
+
+//変数の宣言：身長（m）の代入
+let userHeight = 1.7;
+
+//変数の宣言：BMI＝体重/（身長＊身長）
+let bmi = userWeight / (userHeight * userHeight)
+
+//コンソールへの出力
+console.log(bmi)
